1591916f731a84c65aa3501f462eddde90c758e7
Angie Lara <angielara@unimayor.edu.co>
2026-09-07 00:19:58 -0500

    Se crearon componetes faltantes con dto y entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PlayerModule } from './player/player.module';
+import { TeamModule } from './team/team.module';
+import { RequestModule } from './request/request.module';
+import { TournamentModule } from './tournament/tournament.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, PlayerModule],
+  imports: [PrismaModule, UsersModule, PlayerModule, TeamModule, RequestModule, TournamentModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
